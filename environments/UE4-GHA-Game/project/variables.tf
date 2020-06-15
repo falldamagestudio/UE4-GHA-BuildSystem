c4c01c88bf27b58cc8c973c0e5f57a3f9e3f3c54
Mikael Kalms <mikael@kalms.org>
2020-06-15 13:45:10 +0200

    Project includes standard vars (cloud org, cloud workspace prefix)

--- environments/UE4-GHA-Game/project/variables.tf
+++ environments/UE4-GHA-Game/project/variables.tf
@@ -1,3 +1,11 @@
+variable "terraform_cloud_organization" {
+  type = string
+}
+
+variable "terraform_cloud_workspace_prefix" {
+  type = string
+}
+
 variable "project_id" {
   description = "The GCP project to use for integration tests"
   type        = string
