28681a97742d2cbc76fa7ecb2381bd36962c18bc
Mikael Kalms <mikael@kalms.org>
2020-06-16 20:38:58 +0200

    Added rule for opening up WinRM traffic through firewall
    Packer needs this port to be open

--- environment/project/main.tf
+++ environment/project/main.tf
@@ -8,3 +8,13 @@ provider "google" {
 module "google_apis" {
   source = "./google_apis"
 }
+
+resource "google_compute_firewall" "allow_winrm" {
+  name    = "allow-winrm"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5986"]
+  }
+}
