09d9236a3c903269922837b4552e635252d2442a
Mikael Kalms <mikael@kalms.org>
2020-06-29 17:09:55 +0200

    Removed obsolete scripts

--- scripts/Remove-TerraformCloudWorkspace.ps1
+++
@@ -1,9 +0,0 @@
-function Remove-TerraformCloudWorkspace {
-
-    param (
-		[Parameter(Mandatory)] [string] $TerraformWorkspaceId,
-		[Parameter(Mandatory)] [string] $TerraformAuthToken
-    )
-    
-    Invoke-RestMethod -UseBasicParsing -Method Delete -Uri "https://app.terraform.io/api/v2/workspaces/${TerraformWorkspaceId}" -ContentType "application/vnd.api+json" -Headers @{ "Authorization" = "Bearer ${TerraformAuthToken}"} | Out-Null
-}
